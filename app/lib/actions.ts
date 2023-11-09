da65cf495b3d1352ec3fbad3d3abc2dc80b3f242
simonbjerkas <simon.bjerkas10@gmail.com>
2023-11-09 13:46:58 +0100

    metadata and authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -115,4 +116,18 @@ export async function deleteInvoice(id: string) {
   }
 
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialSignin')) {
+      return 'CredentialSignin';
+    }
+    throw error;
+  }
 }
